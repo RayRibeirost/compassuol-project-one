f103e5ad5af289bd14fa1223cea5ff86e03ebd50
Rayribeirost <ribeiroraylander7@gmail.com>
2024-11-02 15:21:43 -0300

    home page stylized, login validation function added, json creaated, home offer card script created

--- src/scripts/index/script.js
+++ src/scripts/index/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const offerContainer = document.getElementById("offers-container")
+fetch('src/json/data.json').then(response => response.json())
+.then(offers => offers.forEach(offer => {
+    const card = document.createElement("div");
+    card.classList.add('offers-card');
+    card.innerHTML = `
+        <figure id="card-img" style="background-image:url(${offer.image}); width:292px; height:197px;">
+        </figure>
+        <p class="offers-card-price"><span class="offer-price">${offer.price}</span></p>
+        <p class="offers-card-size"><span class="offer-size">${offer.size}</span> m<sup>2</sup></p>
+        <p class="offers-card-info"><span class="offers-card-info-bedroom"><span class="offer-bedroom">${offer.bedrooms}</span> quartos</span> <span class="offers-card-info-bathroom"><span class="offer-bathroom">${offer.bathrooms}</span> banheiros</span></p>
+    `
+    offerContainer.appendChild(card)
+}));
+
--- src/scripts/login/script.js
+++ src/scripts/login/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function validation() {
+
+    const emailInput = document.getElementById('email').value;
+    const passwordInput = document.getElementById('password').value;
+    const message = document.getElementById('validation-text');
+    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    if (!emailInput && !passwordInput) {
+        message.style.color = "red";
+        message.innerHTML = "Insira seu email e senha";
+    } else if (!emailInput.match(pattern)){
+        message.style.color = "red";
+        message.innerHTML = "Email ou senha inválidos";
+    } else if (passwordInput.length < 8) {
+        message.style.color = "red";
+        message.innerHTML = "Sua senha deve ter 8 caracteres ou mais";
+    } else {
+        message.style.color = "green";
+        message.innerHTML = "Dados ok";
+        window.location.href = "index.html";
+    };
+};
